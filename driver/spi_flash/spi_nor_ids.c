445fc5094058a2a4b6a754f5ea1dd70d1fb5a531
Erik Ahlén <erik.ahlen@rco.se>
2021-12-16 07:45:43 +0100

    spi_flash: spi_nor_ids: Add support for mx66lm1g45g/mx25l51245g
    Same ID on both mx66lm1g45g and mx25l51245g. For details see:
    https://lore.kernel.org/linux-mtd/20210405083208.qhdvpokoyr36awzf@ti.com/T/
    And some of the problems regarding 3 and 4 byte opcodes:
    https://linux-mtd.infradead.narkive.com/90mgDZkV/patch-mtd-spi-nor-fix-options-for-mx66l51235f
    
    Signed-off-by: Erik Ahlén <erik.ahlen@rco.se>

--- driver/spi_flash/spi_nor_ids.c
+++ driver/spi_flash/spi_nor_ids.c
@@ -49,6 +49,7 @@
 const struct spi_nor_info spi_nor_ids[] = {
 	/* Macronix */
 	{ MX25("mx25l25635f", 0xc22019,  512), },
+	{ MX66("mx66l51235f", 0xc2201a, 1024), },
 	{ MX66("mx66lm1g45g", 0xc2853b, 2048), },
 
 	/* Micron */
